d63701ae60fce759ec1a684f495d91bfe8b1264a
pbartkowicz <pat.bartkowicz@gmail.com>
2020-04-19 19:39:45 +0200

    Change function to have one responsibility

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,15 @@ import (
 )
 
 func main() {
-	// p, err := open("../data/students/groups.xlsx")
-	// if err != nil {
-	// 	fmt.Printf("An error occurred: %s", err.Error())
-	// 	return
-	// }
-	// if err := convert.Convert(p); err != nil {
-	// 	fmt.Printf("An error occurred: %s", err.Error())
-	// }
-	if err := xlsx.Read("data/groups.xlsx", xlsx.Group); err != nil {
+	g, err := xlsx.Read("data/groups.xlsx")
+	if err != nil {
 		fmt.Printf("An error occurred: %s", err.Error())
+		return
+	}
+	for _, gg := range g {
+		for _, ggg := range gg {
+			fmt.Printf("%v", ggg)
+		}
+		fmt.Printf("\n")
 	}
 }
-
-// func open(f string) (string, error) {
-// 	return filepath.Abs(f)
-// }
--- internal/xlsx/read.go
+++ internal/xlsx/read.go
@@ -3,74 +3,34 @@ package xlsx
 
 import (
 	"errors"
-	"fmt"
 	"path/filepath"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
-	"github.com/pbartkowicz/scheduler/internal/schedule"
-)
-
-// ReadOptions ...
-type ReadOptions string
-
-const (
-	Group            ReadOptions = "Group"
-	PriorityStudents ReadOptions = "Priority Students"
-	Student          ReadOptions = "Student"
 )
 
 // Read ...
-func Read(n string, op ReadOptions) error {
+func Read(n string) ([][]string, error) {
+	data := make([][]string, 0)
 	p, err := filepath.Abs(n)
 	if err != nil {
-		return err
+		return data, err
 	}
 	f, err := excelize.OpenFile(p)
 	if err != nil {
-		return err
+		return data, err
 	}
 	s := f.GetSheetName(1)
 	if s == "" {
-		return errors.New("invalid")
+		return data, errors.New("invalid")
 	}
 	rows, err := f.Rows(s)
 	if err != nil {
-		return errors.New("invalid")
+		return data, errors.New("invalid")
 	}
-	/*if !rows.Next() {
-		return errors.New("invalid")
-	}
-	names := rows.Columns()*/
-	/*for _, ff := range names {
-		fmt.Printf("%v\n", ff)
-	}*/
-	/*for rows.Next() {
-		row := rows.Columns()
-		for _, col := range row {
-			fmt.Printf("%T %v\n", col, col)
-		}
-	}*/
-	switch op {
-	case Group:
-		createGroups(rows)
-	}
-	return nil
-}
-
-func createGroups(rows *excelize.Rows) error {
-	// TODO - firstly create subjects
-	if !rows.Next() {
-		return errors.New("invalid")
-	}
-	//names := rows.Columns()
+	// Skip first line
+	rows.Next()
 	for rows.Next() {
-		row := rows.Columns()
-		g := schedule.NewGroup(row)
-		fmt.Printf("%+v", g)
+		data = append(data, rows.Columns())
 	}
-	return nil
+	return data, nil
 }
-
-/*func newGroup(names []string, values []string) *schedule.Group {
-	return
-}*/
