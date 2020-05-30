3a0472396f8461c22938ba3feefe9da2933765a1
pbartkowicz <pat.bartkowicz@gmail.com>
2020-05-30 15:07:26 +0200

    Fix assigning students to groups

--- internal/schedule/schedule.go
+++ internal/schedule/schedule.go
@@ -1,15 +1,22 @@
 package schedule
 
-import "github.com/pbartkowicz/scheduler/internal/university"
+import (
+	"github.com/pbartkowicz/scheduler/internal/university"
+)
 
 // Enroll ... - move to university?
 func Enroll(schedule *university.Schedule, students []*university.Student) error {
+	// Assign students to preferred groups
 	for _, st := range students {
 		for _, s := range schedule.Subjects {
 			for _, l := range s.Lectures {
 				l.Students = append(l.Students, st)
 			}
 			gns := s.GetGroupsNames()
+			// Subject has no groups
+			if len(gns) == 0 {
+				continue
+			}
 			prefGroup := st.GetPrefredGroup(s.Name, gns)
 			g := s.GetGroup(prefGroup)
 			if st.Priority {
